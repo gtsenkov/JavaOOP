dbe25aa24a30468fe04858f1cce8dcd6524f8add
gtsenkov <70806756+gtsenkov@users.noreply.github.com>
2020-12-09 14:14:05 +0200

    Add files via upload

---
+++ EncapsulationLab/ValidationData/Person.java
@@ -0,0 +1,75 @@
+package EncapsulationLab.ValidationData;
+
+public class Person {
+
+    private String firstName;
+    private String lastName;
+    private int age;
+    private double salary;
+
+    public Person(String firstName, String lastName, int age, double salary) {
+        this.setFirstName(firstName);
+        this.setLastName(lastName);
+        this.setAge(age);
+        this.setSalary(salary);
+    }
+
+    public void increaseSalary(double percentage) {
+        if (this.getAge() < 30) {
+            this.setSalary(this.getSalary() + (this.getSalary() * percentage / 200));
+        } else {
+            this.setSalary(this.getSalary() + (this.getSalary() * percentage / 100));
+        }
+    }
+
+    public void setAge(int age) {
+        if (age <= 0) {
+            throw new IllegalArgumentException("Age cannot be zero or negative integer");
+        }
+        this.age = age;
+    }
+
+    public void setFirstName(String firstName) {
+        validateNameLength(firstName, "First");
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        validateNameLength(lastName, "Last");
+        this.lastName = lastName;
+    }
+
+    private void validateNameLength(String name, String messagePrefix) {
+        if (name.length() < 3) {
+            throw new IllegalArgumentException(messagePrefix + " name cannot be less than 3 symbols");
+        }
+    }
+
+    private void setSalary(double salary) {
+        if (salary < 460) {
+            throw new IllegalArgumentException("Salary cannot be less than 460 leva");
+        }
+        this.salary = salary;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public String toString() {
+        return firstName + " " + lastName + " gets " + salary + " leva";
+    }
+}
