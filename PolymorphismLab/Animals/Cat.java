c02f4b446345fa50ff4566d0e9cf5bbb3c00b8ba
gtsenkov <70806756+gtsenkov@users.noreply.github.com>
2020-12-12 13:19:19 +0200

    Add files via upload

---
+++ PolymorphismLab/Animals/Cat.java
@@ -0,0 +1,15 @@
+package PolymorphismLab.Animals;
+
+public class Cat extends Animal {
+
+    public Cat(String name, String favouriteFood) {
+        super(name, favouriteFood);
+    }
+
+    @Override
+    public String explainSelf() {
+        return super.toString() +
+                System.lineSeparator() +
+                "MEEOW";
+    }
+}
